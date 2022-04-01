573615a2047eb608c23980a5db87e04ef79982d7
Tom Kruger <krugertom@outlook.com>
2022-04-01 22:15:36 +0800

    Update module_install.py
    Changes to notes.

--- module_install.py
+++ module_install.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 21 14:10:52 2022
 
-@author: yevheniia
-"""
 import os
 os.system("pip install pandas")
 os.system("pip install json")
